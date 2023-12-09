bc43cb8c3c14071bf51aa5bd03a084a75a06f779
Jo Franz <franz@systopia.de>
2023-12-09 16:16:56 +0100

    add vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1cad062787136bee9d6d9d804e201a3b6c7d06a5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1cad062787136bee9d6d9d804e201a3b6c7d06a5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'chillerlan/php-qrcode' => array(
+            'pretty_version' => '2.0.8',
+            'version' => '2.0.8.0',
+            'reference' => 'bf0382aaf2f79fa41c2dcb0f216675f74d633fe7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chillerlan/php-qrcode',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'chillerlan/php-traits' => array(
+            'pretty_version' => '1.1.13',
+            'version' => '1.1.13.0',
+            'reference' => '264759946b6aaeb427346b749fc9639b790b8e7f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chillerlan/php-traits',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
