da5d2b435f1ff0f92df2eb6d7f5110fe9ced81ac
Dominic Tubach <tubach@systopia.de>
2026-02-24 11:59:44 +0100

    Set version to 1.1.0-beta1

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -24,14 +24,14 @@ class ComposerAutoloaderInitdc8b71f0a552adbcaa43d4a63fe006df
 
         require __DIR__ . '/platform_check.php';
 
-        spl_autoload_register(array('ComposerAutoloaderInitdc8b71f0a552adbcaa43d4a63fe006df', 'loadClassLoader'), true, true);
+        spl_autoload_register(array('ComposerAutoloaderInitdc8b71f0a552adbcaa43d4a63fe006df', 'loadClassLoader'), true, false);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitdc8b71f0a552adbcaa43d4a63fe006df', 'loadClassLoader'));
 
         require __DIR__ . '/autoload_static.php';
         call_user_func(\Composer\Autoload\ComposerStaticInitdc8b71f0a552adbcaa43d4a63fe006df::getInitializer($loader));
 
-        $loader->register(true);
+        $loader->register(false);
 
         return $loader;
     }
